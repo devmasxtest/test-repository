56754142cba051fbd610fee731a0b72793813817
Miguel Savignano <migue.masx@gmail.com>
2020-07-22 22:41:40 +0200

    example create check

--- .github/actions/example-action/lib/index.rb
+++ .github/actions/example-action/lib/index.rb
@@ -1,3 +1,29 @@
 require 'json'
+require 'net/http'
+require 'json'
+require 'time'
+require_relative './github_client'
+CHECK_NAME = 'Example'
+
+client = GithubClient.new(ENV['GITHUB_TOKEN'])
+check_id = client.post("/repos/#{ENV['GITHUB_REPOSITORY']}/check-runs", {
+  name: CHECK_NAME,
+  head_sha: ENV['GITHUB_TOKEN'],
+  status: 'in_progress',
+  started_at: Time.now.iso8601
+})['id']
+
+client.patch("/repos/#{ENV['GITHUB_REPOSITORY']}/check-runs/#{check_id}", {
+  name: CHECK_NAME,
+  head_sha: ENV['GITHUB_SHA'],
+  status: 'completed',
+  completed_at: Time.now.iso8601,
+  conclusion: 'Conclusion',
+  output: {
+    title: CHECK_NAME,
+    summary: 'Summary',
+    annotations: []
+  }
+})
 
 puts "Hello world"
